21bd39f7eddb298f2649558ab8e159ac9458ae74
ddnhuy <doan.nhathuy.03@gmail.com>
2025-05-29 23:12:57 +0700

    Bugfix: Fix duplication error for Sonarqube

--- src/Services/Management/Management.API/Users/Commands/UpdateUserCommand.cs
+++ src/Services/Management/Management.API/Users/Commands/UpdateUserCommand.cs
@@ -1,6 +1,4 @@
-﻿using Management.API.Dtos.Department;
-using Management.API.Dtos.DepartmentType;
-using Management.API.Dtos.User;
+﻿using Management.API.Dtos.User;
 using Management.API.Helpers;
 
 namespace Management.API.Users.Commands
@@ -55,38 +53,7 @@ namespace Management.API.Users.Commands
 
             var result = await applicationUserProto.UpdateApplicationUserAsync(request, metadata, cancellationToken: cancellationToken);
 
-            return new UpdateUserResult(new UserDetailDto
-            {
-                Id = result.Id,
-                UserName = result.UserName,
-                Email = result.Email,
-                Code = result.Code,
-                Name = result.Name,
-                PhoneNumber = result.PhoneNumber,
-                EmailConfirmed = result.EmailConfirmed,
-                PhoneNumberConfirmed = result.PhoneNumberConfirmed,
-                TwoFactorEnabled = result.TwoFactorEnabled,
-                IsSuspended = result.IsSuspended,
-                IsCancelled = result.IsCancelled,
-                CreatedAt = result.CreatedAt.ToDateTime(),
-                LastUpdatedAt = result.LastUpdatedAt.ToDateTime(),
-                Roles = result.Roles.Split(',').ToList(),
-                Departments = result.Departments.Select(d => new DepartmentSummaryDto
-                {
-                    Id = d.Id,
-                    Code = d.Code,
-                    Name = d.Name,
-                    NameInEnglish = d.NameInEnglish,
-                    DepartmentType = new DepartmentTypeSummaryDto
-                    {
-                        Name = d.DepartmentTypeName,
-                        NameInEnglish = d.DepartmentTypeNameInEnglish
-                    },
-                    IsSuspended = d.IsSuspended
-                }).ToList(),
-                Address = result.Address,
-                ProfilePictureUrl = result.ProfilePictureUrl
-            });
+            return new UpdateUserResult(ConvertUserHelper.ToUserDetailDto(result));
         }
     }
 }
