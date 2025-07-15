01e1904fafe01cd81da74c354cddc2c1b1b7afca
Byte2003 <byte050403@gmail.com>
2025-07-15 22:35:15 +0700

    Message broker for checking whether stock of medicine is enough and update the inventory after vaccination.

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Contracts/Inventory/MedicineStock/SubtractMedicineBatchStockRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Messaging.Contracts.Inventory.MedicineStock
+{
+    public class SubtractMedicineBatchStockRequest
+    {
+        public int MedicineBatchId { get; init; }
+        public int Quantity { get; init; }
+        public string? RequestId { get; init; } = Guid.NewGuid().ToString();
+        public DateTime RequestedAt { get; init; } = DateTime.UtcNow;
+    }
+
+    public class SubtractMedicineBatchStockResponse
+    {
+        public int MedicineBatchId { get; init; }
+        public int Quantity { get; init; }
+        public bool IsSuccess { get; init; }
+        public string? ErrorMessage { get; init; }
+        public string? RequestId { get; init; }
+        public DateTime RespondedAt { get; init; } = DateTime.UtcNow;
+    }
+}
