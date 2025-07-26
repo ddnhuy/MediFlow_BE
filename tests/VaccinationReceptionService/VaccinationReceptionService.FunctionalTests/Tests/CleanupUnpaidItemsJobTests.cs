846f4c378ed4d0fe061660c3eea9549dc8b6b059
BuuLe-cyper <buule.210303@gmail.com>
2025-07-26 14:25:48 +0700

    Update test coverage sonar

--- tests/VaccinationReceptionService/VaccinationReceptionService.FunctionalTests/Tests/CleanupUnpaidItemsJobTests.cs
+++ tests/VaccinationReceptionService/VaccinationReceptionService.FunctionalTests/Tests/CleanupUnpaidItemsJobTests.cs
@@ -15,48 +15,48 @@ using MockQueryable.Moq;
 
 public class CleanupUnpaidItemsJobTests
 {
-    //[Fact]
-    //public async Task Execute_ShouldCancelUnpaidItemsAndSaveChanges()
-    //{
-    //    // Arrange
-    //    var loggerMock = new Mock<ILogger<CleanupUnpaidItemsJob>>();
+    [Fact]
+    public async Task Execute_ShouldCancelUnpaidItemsAndSaveChanges()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<CleanupUnpaidItemsJob>>();
 
-    //    // Mock data
-    //    var receptionId = 1;
-    //    var receptions = new List<Reception>
-    //    {
-    //        new Reception { Id = receptionId, ReceptionDate = DateTime.UtcNow.AddHours(-6) }
-    //    }.AsQueryable();
+        // Mock data
+        var receptionId = 1;
+        var receptions = new List<Reception>
+        {
+            new Reception { Id = receptionId, ReceptionDate = DateTime.UtcNow.AddHours(-6) }
+        }.AsQueryable();
 
-    //    var receptionVaccinations = new List<ReceptionVaccination>
-    //    {
-    //        new ReceptionVaccination { ReceptionId = receptionId, PaymentStatus = PaymentStatusForItem.NotPaid, IsCancelled = false }
-    //    }.AsQueryable();
+        var receptionVaccinations = new List<ReceptionVaccination>
+        {
+            new ReceptionVaccination { ReceptionId = receptionId, PaymentStatus = PaymentStatusForItem.NotPaid, IsCancelled = false }
+        }.AsQueryable();
 
-    //    var serviceRequestDetails = new List<ServiceRequestDetail>
-    //    {
-    //        new ServiceRequestDetail { ReceptionId = receptionId, PaymentStatus = PaymentStatusForItem.NotPaid, IsCancelled = false }
-    //    }.AsQueryable();
+        var serviceRequestDetails = new List<ServiceRequestDetail>
+        {
+            new ServiceRequestDetail { ReceptionId = receptionId, PaymentStatus = PaymentStatusForItem.NotPaid, IsCancelled = false }
+        }.AsQueryable();
 
-    //    // Mock DbSet
-    //    var receptionsDbSet = receptions.BuildMockDbSet();
-    //    var receptionVaccinationsDbSet = receptionVaccinations.BuildMockDbSet();
-    //    var serviceRequestDetailsDbSet = serviceRequestDetails.BuildMockDbSet();
+        // Mock DbSet
+        var receptionsDbSet = receptions.BuildMockDbSet();
+        var receptionVaccinationsDbSet = receptionVaccinations.BuildMockDbSet();
+        var serviceRequestDetailsDbSet = serviceRequestDetails.BuildMockDbSet();
 
-    //    var dbContextMock = new Mock<IApplicationDbContext>();
-    //    dbContextMock.Setup(x => x.Receptions).Returns(receptionsDbSet.Object);
-    //    dbContextMock.Setup(x => x.ReceptionVaccinations).Returns(receptionVaccinationsDbSet.Object);
-    //    dbContextMock.Setup(x => x.ServiceRequestDetails).Returns(serviceRequestDetailsDbSet.Object);
-    //    dbContextMock.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(2);
+        var dbContextMock = new Mock<IApplicationDbContext>();
+        dbContextMock.Setup(x => x.Receptions).Returns(receptionsDbSet.Object);
+        dbContextMock.Setup(x => x.ReceptionVaccinations).Returns(receptionVaccinationsDbSet.Object);
+        dbContextMock.Setup(x => x.ServiceRequestDetails).Returns(serviceRequestDetailsDbSet.Object);
+        dbContextMock.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(2);
 
-    //    var job = new CleanupUnpaidItemsJob(loggerMock.Object, dbContextMock.Object);
+        var job = new CleanupUnpaidItemsJob(loggerMock.Object, dbContextMock.Object);
 
-    //    // Act
-    //    await job.Execute(Mock.Of<IJobExecutionContext>());
+        // Act
+        await job.Execute(Mock.Of<IJobExecutionContext>());
 
-    //    // Assert
-    //    Assert.True(receptionVaccinations.First().IsCancelled);
-    //    Assert.True(serviceRequestDetails.First().IsCancelled);
-    //    dbContextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
-    //}
+        // Assert
+        Assert.True(receptionVaccinations.First().IsCancelled);
+        Assert.True(serviceRequestDetails.First().IsCancelled);
+        dbContextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
 }
