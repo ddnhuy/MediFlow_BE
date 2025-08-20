d3edb6c8cff5499fa901c8231c492314a50a3dc7
BuuLe-cyper <buule.210303@gmail.com>
2025-08-20 23:50:48 +0700

    Add get all payment api

---
+++ src/Services/VaccinationReception/VaccinationReception.API/EndPoints/HospitalFeeEndpoints/GetAllPaymentsWithPatientsEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using VaccinationReception.Application.DTOs.HospitalFeeDTOs;
+using VaccinationReception.Application.HospitalFees.Queries;
+
+namespace VaccinationReception.API.EndPoints.HospitalFeeEndpoints
+{
+    public record GetAllPaymentsWithPatientsResponse(PaginatedResult<PaymentWithPatientDTO> Payments);
+
+    public class GetAllPaymentsWithPatientsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/payments", async ([AsParameters] PaginationRequest request, string? searchTerm, ISender sender) =>
+            {
+                PaginationHelper.VerifyPaginationRequest(request.PageIndex, request.PageSize);
+
+                var result = await sender.Send(new GetAllPaymentsWithPatientsQuery(request, searchTerm));
+                var response = new GetAllPaymentsWithPatientsResponse(result);
+
+                return Results.Ok(response);
+            })
+            .RequireAuthorization()
+            .WithName("GetAllPaymentsWithPatients")
+            .WithTags("Payment")
+            .Produces<GetAllPaymentsWithPatientsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Get payments with patient info")
+            .WithDescription("Returns payments joined with patient info. Applies search (Name/Code/IdentityCard/InvoiceNumber) at the end, then paginates.");
+        }
+    }
+}
